324ff6d6d7a00bef7ca54d7e01d27ceca6d1282e
Roman Masek <romario333@gmail.com>
2025-07-02 17:16:50 +0200

    wip

--- app.vue
+++ app.vue
@@ -6,9 +6,19 @@ import AppSidebar from "@/components/AppSidebar.vue";
 <template>
   <SidebarProvider>
     <AppSidebar />
-    <main>
-      <SidebarTrigger />
-      <RouterView />
-    </main>
+    <SidebarInset>
+      <header
+        class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
+      >
+        <div class="flex items-center gap-2 px-4">
+          <SidebarTrigger class="-ml-1" />
+          <Separator orientation="vertical" class="mr-2 h-4" />
+          Oliver
+        </div>
+      </header>
+      <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <RouterView />
+      </div>
+    </SidebarInset>
   </SidebarProvider>
 </template>
